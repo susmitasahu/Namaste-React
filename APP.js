3b0713735a85e274ee7a21aa8e19ab676d2acce1
susmita sahu <suslipi.sahu@gmail.com>
2024-11-10 19:26:37 +0530

    episode-01

---
+++ APP.js
@@ -0,0 +1,20 @@
+/*const heading = React.createElement("h1",
+    {id:"heading",xyz:"abc"},
+    "HELLO WORLD");
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);*/
+const parent = React.createElement(
+    "div",
+    {id : "parent"},
+    React.createElement(
+        "div",
+        {id:"child"},
+        [React.createElement("h1",{},"I am in h1 tag"),
+         React.createElement("h2",{},"I am in h2 tag"),
+        ]
+    )
+);
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
